a98ee994f902d45ce586b5c0d381c6677ddd38aa
FlixD78 <142332091+FlixD78@users.noreply.github.com>
2024-08-18 14:38:24 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Component from '@/components/ui/page'; // Ensure this path is correct
+import React from 'react';
+
+function Page() {
+  return (
+    <div className='flex items-center min-h-screen'>
+      <Component />
+    </div>
+  );
+}
+
+export default Page;
