7f5a67219ee619b7e4ef1ef6ae530998bb33a7ff
FlixD78 <142332091+FlixD78@users.noreply.github.com>
2024-09-08 18:34:22 +0100

    hard work

--- src/components/ui/page.tsx
+++ src/components/ui/page.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import HomeTabNav from "../../components/ui/navhaeders/navlinks";
 import styles from "../../app/styles.module.css";
-import PersonCard from '../ui/PersonCard/PersonCard';
+import PersonCard from './PersonCard/PersonCard';
 import AnimatedTabs from "../Taps/job/page";
 
 const Component = () => {
